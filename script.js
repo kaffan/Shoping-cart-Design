f6708b25dd78d401a9d549d54907935958bc5dcc
Affan Khan <affan.khan@ugamsolutions.com>
2022-03-31 20:51:09 +0530

    latest update

---
+++ script.js
@@ -0,0 +1,14 @@
+const head_list = document.getElementById('head');
+
+
+
+
+
+// when screen size resches to 600px
+window.addEventListener('resize',function(e)
+{
+    if(e.target.width==='600px')
+    {
+        head_list.removeChild()
+    }
+});
