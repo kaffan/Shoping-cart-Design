fa92f9afc0250da6e60663c2388319c7092e7159
Affan Khan <affan.khan@ugamsolutions.com>
2022-07-15 21:53:16 +0530

    New Commit

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const head_list = document.getElementById('head');
+const head = document.getElementById('head');
+const headListItem = document.querySelector('.list_item');
+const HeadEnd = document.querySelector('.hd_end');
+const usrCart = document.querySelector('.hd_end .usr_cart')
+const menu = document.querySelector('.menuBtn');
 
-
-
-
-
-// when screen size resches to 600px
-window.addEventListener('resize',function(e)
+// On hover end of heading 
+HeadEnd.addEventListener('mouseover',(e)=>
 {
-    if(e.target.width==='600px')
-    {
-        head_list.removeChild()
-    }
+    e.preventDefault();
+    usrCart.style.display = "block";
 });
+HeadEnd.addEventListener('mouseout',(e)=>
+{
+    e.preventDefault();
+    usrCart.style.display = "none";
+});
+
+
+// when screen size reaches 
+
+window.addEventListener("resize",(e)=>
+{
+    console.log(e.currentTarget.innerWidth)
+    if(e.currentTarget.innerWidth<=743)
+    {
+        headListItem.style.display="none";
+        menu.style.display="block";
+    }
+    else if(e.currentTarget.innerWidth>743)
+    {
+        headListItem.style.display="flex";
+        menu.style.display="none";
+    }
+})
