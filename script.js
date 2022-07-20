44e30b928d4bf9174e3080eb5914f24efd7705d4
kaffan <kaffan022@gmail.com>
2022-07-20 22:50:30 +0530

    Latest Commit 20thjuly 2022

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
+'use strict'
+
 const head = document.getElementById('head');
 const headListItem = document.querySelector('.list_item');
 const HeadEnd = document.querySelector('.hd_end');
 const usrCart = document.querySelector('.hd_end .usr_cart')
 const menu = document.querySelector('.menuBtn');
 
-// On hover end of heading 
-HeadEnd.addEventListener('mouseover',(e)=>
-{
-    e.preventDefault();
-    usrCart.style.display = "block";
-});
-HeadEnd.addEventListener('mouseout',(e)=>
-{
-    e.preventDefault();
-    usrCart.style.display = "none";
-});
+// Application Architecture
 
-
-// when screen size reaches 
-
-window.addEventListener("resize",(e)=>
-{
-    console.log(e.currentTarget.innerWidth)
-    if(e.currentTarget.innerWidth<=743)
-    {
-        headListItem.style.display="none";
-        menu.style.display="block";
+class App{
+    constructor(){
+        HeadEnd.addEventListener('mouseover',this.#whenmouseover.bind(this));
+        HeadEnd.addEventListener('mouseout',this.#whenmouseout.bind(this));
+        this.#windowEvent();
     }
-    else if(e.currentTarget.innerWidth>743)
-    {
-        headListItem.style.display="flex";
-        menu.style.display="none";
+    #whenmouseover(e){
+        e.preventDefault();
+        usrCart.style.display = "block";
     }
-})
+    #whenmouseout(e){
+        e.preventDefault();
+        usrCart.style.display = "none";
+    }
+    #windowEvent(){
+        window.addEventListener('resize',(e)=>{
+            console.log(e.currentTarget.innerWidth)
+            if (e.currentTarget.innerWidth <= 744) {
+                headListItem.style.display = "none";
+                menu.style.display = "block";
+            }
+            else if (e.currentTarget.innerWidth > 744) {
+                headListItem.style.display = "flex";
+                menu.style.display = "none";
+            }
+        });
+    }
+}
+
+const app = new App();
